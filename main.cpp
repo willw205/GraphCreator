710e53e07d42ccb96097e8a1b21b3911f5c0bb6b
William Wong <williamwong@Williams-MacBook-Pro.local>
2023-06-04 09:00:19 -0700

    Finished working on functions and comments

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 
+ //William Wong
+ //6/4/23
+ //This is the graph creator project which creates a Table the displays the labels of the nodes and the weights of the edges between
+
  #include <cstring>
  #include <iostream>
  #include <vector>
@@ -7,10 +11,12 @@
  using namespace std;
 
  struct Node{
-   char* label;
-   vector<Node*> connect;
-   vector<int> weightlist;
+   char* label; //The label of vertex
+   vector<Node*> connect; //the lit of vertices connected toward
+   vector<int> weightlist; //list of weights
  };
+
+ //Function prototypes below
 
  void addVertex(vector<Node*>* glist);
  void addEdge(vector<Node*>* glist);
@@ -23,32 +29,62 @@
  int main() {
 
    vector<Node*> glist;
-   char* userInput = new char[50];
+   char* userInput = new char[20];
    bool cont = true;
    while (cont == true) {
      cout << "Please input one of these commands AV (Add Vertext), AE (Add Edge), RV (Remove Vertex), RE (Remove Edge), SP (Shortest Path) PRINT (Print) QUIT (Quit) " << endl;
      cin >> userInput;
      cin.ignore();
-     if (strcmp(userInput, "AV") == 0) {
+     if (strcmp(userInput, "AV") == 0) { //If user wants to add vertex
        addVertex(&glist);
      }
-     else if (strcmp(userInput, "AE") == 0) {
+     else if (strcmp(userInput, "AE") == 0) { //If user wants to Add Edge
        addEdge(&glist);
      }
-     else if (strcmp(userInput, "RV") == 0) {
+     else if (strcmp(userInput, "RV") == 0) { //IF user wants to remove vertex
        removeVertex(&glist);
      }
-     else if (strcmp(userInput, "RE") == 0) {
+     else if (strcmp(userInput, "RE") == 0) { //If user wants to remove edge
        removeEdge(&glist);
      }
-     else if (strcmp(userInput, "PRINT") == 0) {
+     else if (strcmp(userInput, "PRINT") == 0) { //If user wants to print
        printTable(glist);
      }
      else if (strcmp(userInput, "SP") == 0) {
-       //write the shortest path stuff here
+       //function error
+       char* ione = new char(50);
+       char* itwo = new char(50);
+       Node* one = NULL;
+       Node* two = NULL;
+       bool yone = false;
+       bool ytwo = false;
+       int i = 0;
+       vector<Node*> :: iterator it;
+       cout << "Vertex 1: " << endl;
+       cin.getline(ione, 50);
+       cout << "Vertex 2: " << endl;
+       cin.getline(itwo, 50);
+       for(it = glist.begin(); it != glist.end(); it++){//check if both vertices exist
+	 if (strcmp((*it)->label, ione) == 0){
+	   one = *(it);
+	   yone = true;
+	 }
+	 if (strcmp((*it)->label, itwo) == 0){
+	   two = *(it);
+	   ytwo = true;
+	 }
+       }
+       if (yone == true && ytwo == true){//if both exist, shortest path
+	 dijkstra(glist, one, two);
+       }
+       else{
+	 cout << "Invalid Vertices!" << endl;
+       }
      }
-     else if (strcmp(userInput, "QUIT") == 0) {
+     else if (strcmp(userInput, "QUIT") == 0) { //If user wants to quit
        cout << "Thanks for using this program!" << endl;
+       cont = false;
+       return 0;
      }
    }
    cout << endl;
@@ -56,39 +92,45 @@
 			      
  }
 
+ //Below helps to return the distance from the current vertex to first element of vertex
+
  int distance(Node* x, Node* y) {
 
    vector<Node*> :: iterator it;
    for(it = x->connect.begin(); it != x->connect.end(); it++) {
-     if ((*it) == y) {
-       return it - x->connect.begin();
+     if ((*it) == y) { //if node is within connected v ertices
+       return it - x->connect.begin(); //returns distance
      }
    }
    return -1;
  }
 
+ //Adds vertext to list 
+
  void addVertex(vector<Node*>* glist) {
 
    Node* temp = new Node();
-   char* input = new char(50);
-   cout << "Vertext Label: " << endl;
+   char* input = new char[50];
+   cout << "Vertext Label:" << endl;
    cin.getline(input, 50, '\n');
-   temp->label = input;
+   temp->label = input; //pushing temp into vertexlist
    glist->push_back(temp);
    
  }
 
- void addEdge(vector<Node*>* glist) {
+ //Below helps to take two vertices and assigns edge wieght between them
 
-   char* sone = new char(50);
-   char* stwo = new char(50);
+ void addEdge(vector<Node*>* glist) { 
+
+   char* sone = new char[50];
+   char* stwo = new char[50];
    bool gone = false;
    bool gtwo = false;
    int weight = 0;
    Node* one = NULL;
    Node* two = NULL;
 
-   if(glist->size() < 2) {
+   if(glist->size() < 2) { //if there are less than two vertices
      cout << "There are not enoguth nodes!" << endl;
      return;
    }
@@ -98,7 +140,7 @@
    cin.getline(sone, 50);
    cout << "Vertex 2: " << endl;
    cin.getline(stwo, 50);
-   for(it = glist->begin(); it != glist->end(); it++) {
+   for(it = glist->begin(); it != glist->end(); it++) { //looks for vetex list
      if (strcmp((*it)->label, sone) == 0) {
        one = (*it);
        gone = true;
@@ -108,19 +150,21 @@
        gtwo = true;
      }
    }
-   if(gone == true && gtwo == true) {
+   if(gone == true && gtwo == true) { //if both are existing assign the weight
      cout << "Edge Weight: " << endl;
      cin >> weight;
      cin.ignore();
-     one->connect.push_back(two);
-     one->weightlist.push_back(weight);
+     one->connect.push_back(two); //Vertex 2 connected toward vertex 1
+     one->weightlist.push_back(weight); //storing weight in list
      return;
    }
    cout << "Not valid Vertices!" << endl;
    
  }
 
- void removeVertex(vector<Node*>* glist) {
+ //Helps to remove the corresponding vertex in vertex in the vertext list from userInput
+
+ void removeVertex(vector<Node*>* glist) { 
 
    char* input = new char[50];
    Node* temp = NULL;
@@ -128,34 +172,36 @@
    cout << "Vertex Label: " << endl;
    cin.getline(input, 50);
    vector<Node*> :: iterator it;
-   for (it = glist->begin(); it != glist->end(); it++) {
+   for (it = glist->begin(); it != glist->end(); it++) { //helps to look for the vertex to be deleted
      if (strcmp((*it)->label, input) == 0) {
-       temp = *(it);
+       temp = *(it); //stroing the address
      }
    }
 
-   if (temp == NULL) {
+   if (temp == NULL) { //if the vertex not idnetified
      cout << "Not a valid node! " << endl;
      return;
    }
 
    vector<Node*> :: iterator ite;
-   for (ite = glist->begin(); ite != glist->end(); ite++) {
-     n = distance((*ite), temp);
+   for (ite = glist->begin(); ite != glist->end(); ite++) { //runs through glist
+     n = distance((*ite), temp); //if tem is within, won't return -1
      if (n != -1) {
-       (*ite)->connect.erase((*ite)->connect.begin() + n);
+       (*ite)->connect.erase((*ite)->connect.begin() + n); // connect and weightlist have same distance
        (*ite)->weightlist.erase((*ite)->weightlist.begin() + n);
      }
    }
      vector<Node*> :: iterator iter;
      for(iter = glist->begin(); iter != glist->end(); iter++) {
-       if (temp == (*iter)) {
-	 glist->erase(iter);
+       if (temp == (*iter)) { //goes toward vertex in glist
+	 glist->erase(iter); //helps to clear the memory
 	 iter--;
 	 delete temp;
        }
      }
  }
+
+ //Below helps to remove vertex from connection lists and weight lists
 
  void removeEdge(vector<Node*>* glist) {
 
@@ -168,10 +214,10 @@
    int i = 0;
    vector<Node*> :: iterator it;
    cout << "Vertex 1: " << endl;
-   cin.getline(sone, 50);
+   cin.getline(sone, 50); 
    cout << "Vertex 2: " << endl;
    cin.getline(stwo, 50);
-   for(it = glist->begin(); it != glist->end(); it++) {
+   for(it = glist->begin(); it != glist->end(); it++) { //checks if both vertices exist
      if(strcmp((*it)->label, sone) == 0) {
        one = *(it);
        gone = true;
@@ -181,10 +227,10 @@
        gtwo = true;
      }
    }
-   if (gone == true && gtwo == true) {
-     i = distance(one, two);
+   if (gone == true && gtwo == true) { //if both do exist
+     i = distance(one, two); //if they are connected
      if(i != -1) {
-       one->connect.erase(one->connect.begin() + i);
+       one->connect.erase(one->connect.begin() + i); //helps to erase vertex in connect list and wieght list
        one->weightlist.erase(one->weightlist.begin() + i);
        return;
      }
@@ -193,30 +239,32 @@
    
  }
 
+ //Below helps to print the table
+
  void printTable(vector<Node*> glist) {
 
    vector<Node*> :: iterator it;
    vector<Node*> :: iterator i;
    vector<Node*> :: iterator j;
    int p = 0;
-   cout << "The Adjacency Table: " << endl << endl;
+   cout << "The Adjacency Table:" << endl << endl;
    cout << "\t";
-   for (it = glist.begin(); it != glist.end(); it++) {
+   for (it = glist.begin(); it != glist.end(); it++) { //the first row headers
      cout << (*it)->label;
      cout << "\t";
    }
    cout << endl;
 
-   for(i = glist.begin(); i!=glist.end(); i++) {
-     cout << (*it)->label;
+   for(i = glist.begin(); i!= glist.end(); i++) { //goes through the vertex list
+     cout << (*i)->label; //prints the labels
      cout << "\t";
-     for (j = glist.begin(); j != glist.end(); j++) {
-       p = distance((*i),(*j));
-       if (p != -1) {
-         cout << (*i)->weightlist[p];
+     for (j = glist.begin(); j != glist.end(); j++) { //helps to check for connection/weight between
+       p = distance((*i),(*j)); //checkign for connection/distance of the weight
+       if (p != -1) { //if they are connected
+         cout << (*i)->weightlist[p]; //prints the weight of connected vertices
          cout << "\t";
        }
-       else {
+       else { //if there is no weight, print *
          cout << "*";
          cout << "\t";
       }
@@ -225,8 +273,8 @@
    }
  }
 
- void dijkstrea(vector<Node*> glist, Node* start, Node* stop) {
+ void dijkstra(vector<Node*> glist, Node* start, Node* stop) {
 
-   cout << "Working " << endl;
+   cout << "Working" << endl;
    
  }
